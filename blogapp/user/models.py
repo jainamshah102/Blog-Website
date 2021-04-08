31fae909d69f9ad5ea4f671f1ead50f49a18a9e2
Jainam Shah <61734413+jainam2385@users.noreply.github.com>
2021-04-08 23:59:13 +0530

    Fixed image uploads

--- blogapp/user/models.py
+++ blogapp/user/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-
 from .managers import CustomUserManager
+import uuid
+import os
 
 GENDER_CHOICES = (
     ('M', 'Male'),
@@ -13,13 +14,19 @@ GENDER_CHOICES = (
 )
 
 
+def rename_file_upload(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('avatars/', filename)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
     gender = models.CharField('gender', max_length=1, choices=GENDER_CHOICES)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(upload_to=rename_file_upload, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
