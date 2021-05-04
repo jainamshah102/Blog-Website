c0409862f11025bdd65e37b53c7244bdaed210c9
Jainam Shah <61734413+jainam2385@users.noreply.github.com>
2021-05-04 13:16:10 +0530

    Update views.py

--- blogapp/user/views.py
+++ blogapp/user/views.py
@@ -127,8 +127,9 @@ def view_profile(request, email=None):
             user = User.objects.get(email=email)
             blogs = Blog.objects.filter(author=user,  status=1)
 
-            notify.send(request.user, recipient=user,
-                        verb='viewed your profile')
+            if request.user != user:
+                notify.send(request.user, recipient=user,
+                            verb='viewed your profile')
 
             return render(request, 'view_profile.html', {'user': user, 'blogs': blogs, "current_user": request.user})
 
